085834cf52805a0f9bb3e8969ff94d505f159b55
Max Eingorn <meingorn@gmail.com>
2017-02-03 21:16:20 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join()
+    array.join(" and ")
+  else
+    last = array.pop
+    array_string = array.join(", ")
+    array_string << ", and #{last}"
+  end
 end
